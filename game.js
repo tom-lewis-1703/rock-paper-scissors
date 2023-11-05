bf2c682c225d74de14a275acd9bcc26d979e826d
tom-lewis-1703 <tolewwy@gmail.com>
2023-11-04 21:59:05 -0500

    Fixed unreferenced 'f' error and upper case choice on draw

--- game.js
+++ game.js
@@ -13,33 +13,29 @@ function get_cpu_input(){
 function game(){
     let user_input = get_user_input()
     let cpu_input = get_cpu_input()
-    let cont = "y"
 
-    do {
-        if (user_input == cpu_input){
-            console.log(f`Draw!\nYou both chose ${user_input}`)
-        } else if (user_input == "ROCK" && cpu_input == "SCISSORS"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You win!")
-        } else if (user_input == "ROCK" && cpu_input == "PAPER"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You lose...")
-        } else if (user_input == "SCISSORS" && cpu_input == "ROCK"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You lose...")
-        } else if (user_input == "SCISSORS" && cpu_input == "PAPER"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You win!")
-        } else if (user_input == "PAPER" && cpu_input == "ROCK"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You win!")
-        } else if (user_input == "PAPER" && cpu_input == "SCISSORS"){
-            console.log(f`The cpu chose ${cpu_input.toLowerCase()}.\n`)
-            console.log("You lose...")
-        }
-
-        cont = prompt("Would you like to play again? (y/n)\n")
-    } while (cont == 'y');
+    if (user_input == cpu_input){
+        console.log(`Draw!\nYou both chose ${user_input.toLowerCase()}`)
+    } else if (user_input == "ROCK" && cpu_input == "SCISSORS"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You win!")
+    } else if (user_input == "ROCK" && cpu_input == "PAPER"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You lose...")
+    } else if (user_input == "SCISSORS" && cpu_input == "ROCK"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You lose...")
+    } else if (user_input == "SCISSORS" && cpu_input == "PAPER"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You win!")
+    } else if (user_input == "PAPER" && cpu_input == "ROCK"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You win!")
+    } else if (user_input == "PAPER" && cpu_input == "SCISSORS"){
+        console.log(`The cpu chose ${cpu_input.toLowerCase()}.\n`)
+        console.log("You lose...")
+    }
+    
 }
 
 game()
